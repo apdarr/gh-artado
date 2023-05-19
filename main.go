5322f409a0583d57a610266d0a53feedc0622498
Alex Darr <alexdarr@gmail.com>
2023-05-19 15:26:57 +0200

    List subcommand usage information

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/base64"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+
+	"github.com/cli/go-gh/v2/pkg/repository"
+	"github.com/spf13/cobra"
 )
 
 type Connection struct {
@@ -53,44 +56,91 @@ func returnURlBody(operation, url string) string {
 	return string(body)
 }
 
-func main() {
-	fmt.Println("hi world, this is the gh-artado extension!")
+func _main() error {
+	var repo repository.Repository
 
-	adoResponse := returnURlBody("GET", "https://dev.azure.com/ursa-minus/ursa/_apis/githubconnections?api-version=7.1-preview")
-
-	fmt.Println(adoResponse)
-
-	fmt.Println("test ^^ test")
-
-	// Parse JSON response
-
-	var jsonResponse Response
-
-	if err := json.Unmarshal([]byte(adoResponse), &jsonResponse); err != nil {
-		fmt.Println("Error parsing JSON:", err)
+	rootCmd := &cobra.Command{
+		Use:   "artado <subcommand> [flags]",
+		Short: "gh artado",
 	}
 
-	if len(jsonResponse.Value) > 0 {
-		connectionID := jsonResponse.Value[0].ID
-		fmt.Println("Connection ID:", connectionID)
+	repoOverride := rootCmd.PersistentFlags().StringP("repo", "R", "", "Repository to use in OWNER/REPO format")
 
-		// Get the list of repositories for the connection
-		connectedRepos := "https://dev.azure.com/ursa-minus/ursa/_apis/githubconnections/%s/repos?api-version=7.1-preview"
-		connectedReposUrl := fmt.Sprintf(connectedRepos, connectionID)
-
-		adoResponse := returnURlBody("GET", connectedReposUrl)
-
-		fmt.Println("adoResponse", adoResponse)
-
-		if err := json.Unmarshal([]byte(adoResponse), &jsonResponse); err != nil {
-			fmt.Println("Error parsing JSON:", err)
+	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) (err error) {
+		if *repoOverride != "" {
+			repo, err = repository.Parse(*repoOverride)
+		} else {
+			repo, err = repository.Current()
 		}
-
-		repoUrl := jsonResponse.Value[0].ConnectedRepos
-
-		fmt.Println("Connected repos:", repoUrl)
-
-	} else {
-		fmt.Println("No connections found")
+		return
 	}
+
+	listConnectionsCmd := &cobra.Command{
+		Use:   "list [flags]",
+		Short: "List GitHub connection IDs for a given Azure DevOps board",
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			fmt.Printf("Connection IDs:")
+			return
+		},
+	}
+
+	addRepoCmd := &cobra.Command{
+		Use:   "repo [flags]",
+		Short: "Add a repo to a given connection",
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			fmt.Printf("Added repo %s to connection\n", repo)
+			return
+		},
+	}
+
+	rootCmd.AddCommand(listConnectionsCmd)
+	rootCmd.AddCommand(addRepoCmd)
+
+	return rootCmd.Execute()
+}
+
+func main() {
+	if err := _main(); err != nil {
+		fmt.Fprintln(os.Stderr, "X %s", err.Error())
+	}
+
+	// fmt.Println("hi world, this is the gh-artado extension!")
+
+	// adoResponse := returnURlBody("GET", "https://dev.azure.com/ursa-minus/ursa/_apis/githubconnections?api-version=7.1-preview")
+
+	// fmt.Println(adoResponse)
+
+	// fmt.Println("test ^^ test")
+
+	// // Parse JSON response
+
+	// var jsonResponse Response
+
+	// if err := json.Unmarshal([]byte(adoResponse), &jsonResponse); err != nil {
+	// 	fmt.Println("Error parsing JSON:", err)
+	// }
+
+	// if len(jsonResponse.Value) > 0 {
+	// 	connectionID := jsonResponse.Value[0].ID
+	// 	fmt.Println("Connection ID:", connectionID)
+
+	// 	// Get the list of repositories for the connection
+	// 	connectedRepos := "https://dev.azure.com/ursa-minus/ursa/_apis/githubconnections/%s/repos?api-version=7.1-preview"
+	// 	connectedReposUrl := fmt.Sprintf(connectedRepos, connectionID)
+
+	// 	adoResponse := returnURlBody("GET", connectedReposUrl)
+
+	// 	fmt.Println("adoResponse", adoResponse)
+
+	// 	if err := json.Unmarshal([]byte(adoResponse), &jsonResponse); err != nil {
+	// 		fmt.Println("Error parsing JSON:", err)
+	// 	}
+
+	// 	repoUrl := jsonResponse.Value[0].ConnectedRepos
+
+	// 	fmt.Println("Connected repos:", repoUrl)
+
+	// } else {
+	// 	fmt.Println("No connections found")
+	// }
 }
